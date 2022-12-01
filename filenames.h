0f505f80f298fabede4a0fc637bb775b2534c280
m0in92 <moinahmed100@gmail.com>
2022-11-30 21:50:26 -0500

    Nov 30,2022 21:45

---
+++ filenames.h
@@ -0,0 +1,13 @@
+//
+// Created by moina on 11/30/2022.
+//
+
+#ifndef CSV_DATA_PARSE_EXAMPLE_FILENAMES_H
+#define CSV_DATA_PARSE_EXAMPLE_FILENAMES_H
+
+#include <filesystem>
+#include <string>
+
+extern std::string filePath2 = "../param_neg-electrode.csv";
+
+#endif //CSV_DATA_PARSE_EXAMPLE_FILENAMES_H
